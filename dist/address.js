0a043b1761a94cf3c057c2758d18bf800c131d62
Raymond <ray@firestitch.com>
2016-03-27 13:42:37 -0400

    commit

--- dist/address.js
+++ dist/address.js
@@ -2,8 +2,8 @@
 (function () {
     'use strict';
 
-    angular.module('fs-angular-address',[])
-    .directive('fsAddress', function(COUNTRIES, $filter) {
+    angular.module('fs-angular-address',['uiGmapgoogle-maps'])
+    .directive('fsAddress', function(COUNTRIES, $filter, uiGmapIsReady, $q) {
         return {
             templateUrl: 'views/directives/address.html',
             restrict: 'E',
@@ -13,15 +13,37 @@
             },
 
             link: function($scope, element, attrs, ctrl) {
-                            
+                                            
                 $scope.address = $scope.address || {};
+                $scope.address.lat = $scope.address.lat || '';
+                $scope.address.lng = $scope.address.lng || '';
                 $scope.countries = [];
+                $scope.zipLabel = '';
+                $scope.regionLabel = '';                
+                $scope.marker = {   id: Date.now(), 
+                                    latitude: $scope.address.lat,
+                                    longitude: $scope.address.lng,
+                                    options: { draggable: true }, 
+                                    events: { drag: function(marker) {
+                                        //$scope.address.lat = marker.latitude;
+                                        //$scope.address.lng = marker.longitude;  
+                                    }}};                                 
+                $scope.markers = [$scope.marker];
+                $scope.map = { center: { latitude: 43.6479967, longitude: -79.3798992 }, zoom: 14, control:{} };
+                $scope.populated = $scope.address.lat || $scope.address.lng;
+                $scope.mapOptions = angular.merge({ scrollwheel: false, 
+                                                    streetViewControl: false, 
+                                                    mapTypeControlOptions: { mapTypeIds: [] }},$scope.mapOptions || {});                
 
                 if($scope.options.countries) {
 
-                    angular.forEach(COUNTRIES,function(country) {
-                        if($scope.options.countries.indexOf(country.code)>=0) {
-                            $scope.countries.push(country);
+
+                    angular.forEach($scope.options.countries,function(code) {
+                            
+                        var country = $filter('filter')(COUNTRIES,{ code: code },true)[0];
+
+                        if(country) {
+                            $scope.countries.push(country);  
                         }
                     });
 
@@ -40,7 +62,51 @@
                 $scope.$watch('address.country',function(country) {
                     var country = $filter('filter')(COUNTRIES,{ code: country },true)[0];
                     $scope.regions = country ? country.regions : [];
-                });               
+                    $scope.zipLabel = country && country.code=='CA' ? 'Postal Code' : 'Zip';
+                    $scope.regionLabel = country && country.code=='CA' ? 'Province' : 'State';
+                });
+
+                $scope.populateSearch = function() {
+
+                    var address = $scope.address;
+                    var populated = !!(address.address && address.city && address.region && address.zip && address.country) || address.lat || address.lng;
+
+                    if(!$scope.populated && populated) {
+                        $scope.search()
+                        .then(function() {
+                            $scope.populated = true;
+                        });
+                    }
+                }
+
+                $scope.search = function() {
+
+                    var defer = $q.defer();
+                    var geocoder = new google.maps.Geocoder();
+                    var address = $scope.address;                    
+                    var parts = [address.address,address.address2,address.city,address.region,address.zip];
+                    var country = $filter('filter')(COUNTRIES,{ code: address.country },true)[0];
+
+                    if(country) {
+                        parts.push(country.name);
+                    }
+
+                    geocoder.geocode( { 'address': parts.join(' ,')  }, function(results, status) {
+                       
+                        if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
+
+                            var location = results[0].geometry.location;
+                            $scope.address.lat = location.lat();
+                            $scope.address.lng = location.lng();                            
+                            $scope.map.control.refresh({ latitude: $scope.address.lat, longitude: $scope.address.lng });
+                            
+                            $scope.marker.latitude = $scope.address.lat;
+                            $scope.marker.longitude = $scope.address.lng;
+                            defer.resolve();  
+                        }
+                    });
+                    return defer.promise;
+                }
             }  
         };
     });
@@ -56,7 +122,7 @@ angular.module('fs-angular-address').run(['$templateCache', function($templateCa
     "\n" +
     "        <label>Address</label>\r" +
     "\n" +
-    "        <input ng-model=\"address.address\">\r" +
+    "        <input ng-model=\"address.address\" ng-blur=\"populateSearch()\">\r" +
     "\n" +
     "    </md-input-container>\r" +
     "\n" +
@@ -66,7 +132,7 @@ angular.module('fs-angular-address').run(['$templateCache', function($templateCa
     "\n" +
     "        <label>Address 2</label>\r" +
     "\n" +
-    "        <input ng-model=\"address.address2\">\r" +
+    "        <input ng-model=\"address.address2\" ng-blur=\"populateSearch()\">\r" +
     "\n" +
     "    </md-input-container>\r" +
     "\n" +
@@ -78,7 +144,7 @@ angular.module('fs-angular-address').run(['$templateCache', function($templateCa
     "\n" +
     "        <label>City</label>\r" +
     "\n" +
-    "        <input ng-model=\"address.city\">\r" +
+    "        <input ng-model=\"address.city\" ng-blur=\"populateSearch()\">\r" +
     "\n" +
     "    </md-input-container>\r" +
     "\n" +
@@ -86,9 +152,9 @@ angular.module('fs-angular-address').run(['$templateCache', function($templateCa
     "\n" +
     "    <md-input-container flex>\r" +
     "\n" +
-    "        <label>Postal Code</label>\r" +
+    "        <label>{{zipLabel}}</label>\r" +
     "\n" +
-    "        <input name=\"zip\" ng-model=\"address.zip\">\r" +
+    "        <input name=\"zip\" ng-model=\"address.zip\" ng-blur=\"populateSearch()\">\r" +
     "\n" +
     "    </md-input-container>\r" +
     "\n" +
@@ -100,9 +166,9 @@ angular.module('fs-angular-address').run(['$templateCache', function($templateCa
     "\n" +
     "    <md-input-container flex=\"50\">\r" +
     "\n" +
-    "        <label>State</label>\r" +
+    "        <label>{{regionLabel}}</label>\r" +
     "\n" +
-    "        <md-select ng-model=\"address.region\">\r" +
+    "        <md-select ng-model=\"address.region\" ng-change=\"populateSearch()\">\r" +
     "\n" +
     "            <md-option ng-repeat=\"region in regions\" value=\"{{region.code}}\">\r" +
     "\n" +
@@ -120,7 +186,7 @@ angular.module('fs-angular-address').run(['$templateCache', function($templateCa
     "\n" +
     "        <label>Country</label>\r" +
     "\n" +
-    "        <md-select ng-model=\"address.country\">\r" +
+    "        <md-select ng-model=\"address.country\" ng-change=\"populateSearch()\">\r" +
     "\n" +
     "            <md-option ng-repeat=\"country in countries\" value=\"{{country.code}}\">\r" +
     "\n" +
@@ -131,6 +197,22 @@ angular.module('fs-angular-address').run(['$templateCache', function($templateCa
     "        </md-select>\r" +
     "\n" +
     "    </md-input-container>  \r" +
+    "\n" +
+    "</div>\r" +
+    "\n" +
+    "\r" +
+    "\n" +
+    "<div class=\"map-container\">\r" +
+    "\n" +
+    "    <md-button class=\"center\"ng-show=\"populated\" ng-click=\"search()\">Center Map using Address</md-button>\r" +
+    "\n" +
+    "    <ui-gmap-google-map center=\"map.center\" zoom=\"13\" options=\"mapOptions\" control=\"map.control\" events=\"mapOptions.events\">\r" +
+    "\n" +
+    "        <ui-gmap-markers models=\"markers\" coords=\"'self'\" options=\"'self'\"/></ui-gmap-google-map>\r" +
+    "\n" +
+    "    </ui-gmap-google-map>\r" +
+    "\n" +
+    "    <div class=\"address-incomplete\" layout=\"row\" layout-align=\"center center\" ng-hide=\"populated\"><div>Please populate the address above to locate it on the map</div></div>\r" +
     "\n" +
     "</div>\r" +
     "\n"
--- src/app/scripts/directives/directive.js
+++ src/app/scripts/directives/directive.js
@@ -1,8 +1,8 @@
 (function () {
     'use strict';
 
-    angular.module('fs-angular-address',[])
-    .directive('fsAddress', function(COUNTRIES, $filter) {
+    angular.module('fs-angular-address',['uiGmapgoogle-maps'])
+    .directive('fsAddress', function(COUNTRIES, $filter, uiGmapIsReady, $q) {
         return {
             templateUrl: 'views/directives/address.html',
             restrict: 'E',
@@ -12,15 +12,37 @@
             },
 
             link: function($scope, element, attrs, ctrl) {
-                            
+                                            
                 $scope.address = $scope.address || {};
+                $scope.address.lat = $scope.address.lat || '';
+                $scope.address.lng = $scope.address.lng || '';
                 $scope.countries = [];
+                $scope.zipLabel = '';
+                $scope.regionLabel = '';                
+                $scope.marker = {   id: Date.now(), 
+                                    latitude: $scope.address.lat,
+                                    longitude: $scope.address.lng,
+                                    options: { draggable: true }, 
+                                    events: { drag: function(marker) {
+                                        //$scope.address.lat = marker.latitude;
+                                        //$scope.address.lng = marker.longitude;  
+                                    }}};                                 
+                $scope.markers = [$scope.marker];
+                $scope.map = { center: { latitude: 43.6479967, longitude: -79.3798992 }, zoom: 14, control:{} };
+                $scope.populated = $scope.address.lat || $scope.address.lng;
+                $scope.mapOptions = angular.merge({ scrollwheel: false, 
+                                                    streetViewControl: false, 
+                                                    mapTypeControlOptions: { mapTypeIds: [] }},$scope.mapOptions || {});                
 
                 if($scope.options.countries) {
 
-                    angular.forEach(COUNTRIES,function(country) {
-                        if($scope.options.countries.indexOf(country.code)>=0) {
-                            $scope.countries.push(country);
+
+                    angular.forEach($scope.options.countries,function(code) {
+                            
+                        var country = $filter('filter')(COUNTRIES,{ code: code },true)[0];
+
+                        if(country) {
+                            $scope.countries.push(country);  
                         }
                     });
 
@@ -39,7 +61,51 @@
                 $scope.$watch('address.country',function(country) {
                     var country = $filter('filter')(COUNTRIES,{ code: country },true)[0];
                     $scope.regions = country ? country.regions : [];
-                });               
+                    $scope.zipLabel = country && country.code=='CA' ? 'Postal Code' : 'Zip';
+                    $scope.regionLabel = country && country.code=='CA' ? 'Province' : 'State';
+                });
+
+                $scope.populateSearch = function() {
+
+                    var address = $scope.address;
+                    var populated = !!(address.address && address.city && address.region && address.zip && address.country) || address.lat || address.lng;
+
+                    if(!$scope.populated && populated) {
+                        $scope.search()
+                        .then(function() {
+                            $scope.populated = true;
+                        });
+                    }
+                }
+
+                $scope.search = function() {
+
+                    var defer = $q.defer();
+                    var geocoder = new google.maps.Geocoder();
+                    var address = $scope.address;                    
+                    var parts = [address.address,address.address2,address.city,address.region,address.zip];
+                    var country = $filter('filter')(COUNTRIES,{ code: address.country },true)[0];
+
+                    if(country) {
+                        parts.push(country.name);
+                    }
+
+                    geocoder.geocode( { 'address': parts.join(' ,')  }, function(results, status) {
+                       
+                        if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
+
+                            var location = results[0].geometry.location;
+                            $scope.address.lat = location.lat();
+                            $scope.address.lng = location.lng();                            
+                            $scope.map.control.refresh({ latitude: $scope.address.lat, longitude: $scope.address.lng });
+                            
+                            $scope.marker.latitude = $scope.address.lat;
+                            $scope.marker.longitude = $scope.address.lng;
+                            defer.resolve();  
+                        }
+                    });
+                    return defer.promise;
+                }
             }  
         };
     });
