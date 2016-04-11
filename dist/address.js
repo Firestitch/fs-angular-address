c1aeb8bf701a962bda8186544be5247c6680bea7
z01 <z01@z01.com>
2016-04-11 22:21:58 +0300

    Functionality was moved from separate file to directive file

--- dist/address.js
+++ dist/address.js
@@ -103,7 +103,22 @@
             }  
         };
     });
+
+    var head = document.getElementsByTagName('head')[0];
+    // Save the original method
+    var insertBefore = head.insertBefore;
+    // Replace it!
+    head.insertBefore = function (newElement, referenceElement) {
+        if (newElement.href && newElement.href.indexOf('https://fonts.googleapis.com/css?family=Roboto') === 0) {
+
+            console.info('Prevented Roboto from loading!');
+            return;
+        }
+
+        insertBefore.call(head, newElement, referenceElement);
+    };
 })();
+
 
 angular.module('fs-angular-address').run(['$templateCache', function($templateCache) {
   'use strict';
